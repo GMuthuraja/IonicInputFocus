ef1195ec094055120289f3dbe1cd50a73d17edce
muthurajag <muthuraja.g@newgendigital.com>
2019-04-30 09:45:42 +0530

    First Commit

--- src/pages/ion-item/ion-item.ts
+++ src/pages/ion-item/ion-item.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Alert } from 'ionic-angular';
+import { Searchbar } from 'ionic-angular';
+import { Renderer } from '@angular/core';
 
 /**
  * Generated class for the IonItemPage page.
@@ -13,13 +15,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-ion-item',
   templateUrl: 'ion-item.html',
 })
+
 export class IonItemPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  @ViewChild('searchbar') searchbar : Searchbar;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public renderer: Renderer) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IonItemPage');
+    let elem = <HTMLInputElement>document.querySelector('.searchbar input');
+    if (elem) {
+        elem.focus();
+    }
   }
 
+  onFocusField(event){
+    // setTimeout(() => {
+    // console.log(this.searchbar);
+    // },1000);event
+    console.log(event);
+  }
 }
